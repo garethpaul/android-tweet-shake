45f83aff3c38bc45d13bf7e0c6275b348bd4fc86
Gareth Jones <gjones@quora.com>
2026-06-08 17:41:44 +0000

    test: cover tweet shake debounce

--- app/src/main/java/gpj/tweetshake/ShakeDetector.java
+++ app/src/main/java/gpj/tweetshake/ShakeDetector.java
@@ -4,6 +4,7 @@ final class ShakeDetector {
     static final float GRAVITY_EARTH = 9.80665f;
     static final float SHAKE_THRESHOLD_GRAVITY = 2.0f;
     static final long SHAKE_DEBOUNCE_MILLIS = 200L;
+    private static final float THRESHOLD_EPSILON = 0.0001f;
 
     private long lastShakeAtMillis = -SHAKE_DEBOUNCE_MILLIS;
 
@@ -11,7 +12,7 @@ final class ShakeDetector {
         float accelerationRatio = ((x * x) + (y * y) + (z * z))
                 / (GRAVITY_EARTH * GRAVITY_EARTH);
 
-        if (accelerationRatio < SHAKE_THRESHOLD_GRAVITY) {
+        if (accelerationRatio < SHAKE_THRESHOLD_GRAVITY - THRESHOLD_EPSILON) {
             return false;
         }
 
